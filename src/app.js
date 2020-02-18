2079869023b02a3ca902da150ab56c2cabb87b0b
onitwin <onitwin@gmail.com>
2020-02-18 16:59:02 +0000

    set up all folders and files

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+
+document.addEventListener("DOMContentLoaded",()=>{
+  new Vue({
+    el:"#app",
+    data:{
+      title: "Currency Conversion Website"
+    }
+
+
+
+
+
+  })
+
+
+
+
+
+})
