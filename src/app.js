042cc072a7a16a6e1fbafa4eac3228354c22f874
onitwin <onitwin@gmail.com>
2020-02-18 20:05:33 +0000

    obtained info and displayed

--- src/app.js
+++ src/app.js
@@ -4,7 +4,22 @@ document.addEventListener("DOMContentLoaded",()=>{
   new Vue({
     el:"#app",
     data:{
-      title: "Currency Conversion Website"
+      title: "Currency Conversion Website",
+      currentRates:"Current Exchange Rates",
+      currencies:[],
+      euros:0
+    },
+    mounted(){
+      this.getRates()
+    },
+
+
+    methods:{
+      getRates:function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json()) //for reference- ensure correct arrow syntax used
+        .then(data =>this.currencies=data.rates)
+      }
     }
 
 
